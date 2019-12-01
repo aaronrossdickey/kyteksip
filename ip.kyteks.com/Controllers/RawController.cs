c3c5ba645fa70a2a09ab425c726db79b9ecd1cab
aaron <aaron@kyteks.com>
2019-12-01 16:41:25 -0500

    Added "Raw" view

---
+++ ip.kyteks.com/Controllers/RawController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ip.kyteks.com.Controllers
+{
+    public class RawController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IPAddress remoteIp = Request.HttpContext.Connection.RemoteIpAddress;
+            return Content(remoteIp.ToString());
+        }
+    }
+}
